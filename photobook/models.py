d1aae77181b2f36a513595b659ad3fdaeb8f522f
baidoosik <qoentlr37@naver.com>
2017-05-14 14:41:34 +0900

    model 에 타이틀, 카테고리, 내용 추가, ModelAdmin 적용

--- photobook/models.py
+++ photobook/models.py
@@ -6,4 +6,15 @@ class TimeStamp(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
 class Post(TimeStamp):
+    title = models.CharField(max_length=100,default='photo')
+    category= models.CharField(max_length=100,
+        choices = (
+            ('미국','미국' ),
+            ('은지', '은지'),
+            ('졸업식', '졸업식'),
+        ),default=False)
+    story = models.CharField(max_length=200)
     photo = models.ImageField()
+
+    def __str__(self):
+        return self.title
