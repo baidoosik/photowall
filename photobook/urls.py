19ef9c420ca6ea8ce795df2d296e36b9d1e25025
baidoosik <qoentlr37@naver.com>
2017-05-18 01:43:29 +0900

    메뉴 추가에 html 파일 수정

--- photobook/urls.py
+++ photobook/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns =[
-    url(r'^$',views.home, name='ameria_home'),
+    url(r'^$',views.home, name='lover'),
     url(r'^global_challenge/$',views.global_challenge, name='global_challenge'),
     url(r'^indonesia/$',views.indonesia, name='indonesia'),
     url(r'^mwc/$',views.mwc, name='mwc'),
